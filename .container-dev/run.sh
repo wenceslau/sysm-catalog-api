2f0d8c275fc8f858c5f0fa2aada25f0a79f5a663
Wenceslau Neto <wbaneto@gmail.com>
2025-01-04 18:48:49 +0000

    feat: integrate Kafka ecosystem into development environment
    Add Kafka, Zookeeper, Control Center, and Kafka Connect services to the Docker Compose setup. Include required networks, volumes, and configurations to support Kafka-based applications, enabling message queuing and stream processing capabilities. Update `run.sh` script for seamless container orchestration.

--- .container-dev/run.sh
+++ .container-dev/run.sh
@@ -2,6 +2,10 @@
 echo "Creating network..."
 
 docker network create ntw_elastic
+docker network create ntw_kafka
+docker network create ntw_app
 docker volume create vol_elastic
+docker volume create vol_kafka
+docker volume create vol_kconnect
 
-docker compose up -d elasticsearch
+docker compose up -d
